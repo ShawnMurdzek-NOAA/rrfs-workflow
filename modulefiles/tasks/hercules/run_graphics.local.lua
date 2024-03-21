38f6471fe72b3fa2b2e05f94dc03a26b4383e40f
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2024-03-21 13:11:46 -0500

    Update python environment for pygraf on Hercules
    Updated environment has been tested and successfully works with the python graphics

--- modulefiles/tasks/hercules/run_graphics.local.lua
+++ modulefiles/tasks/hercules/run_graphics.local.lua
@@ -1,3 +1,5 @@
-load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.10.3"))
+unload("python")
+append_path("MODULEPATH","/work2/noaa/wrfruc/murdzek/conda/miniconda_hercules/modulefiles")
+load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "24.1.2"))
 
-setenv("SRW_ENV", "/work2/noaa/wrfruc/murdzek/conda/my_pygraf_hercules")
+setenv("SRW_ENV", "/work2/noaa/wrfruc/murdzek/conda/miniconda_hercules/env/pygraf")
